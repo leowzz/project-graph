2000ddfd11e42b4d3fd0238aa409bf49c7e6c61b
zty012 <z@2y.nz>
2026-05-04 21:41:18 +0800

    chore: 更新依赖

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,6 @@ import pluginJs from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 import pluginReact from "eslint-plugin-react";
-import storybook from "eslint-plugin-storybook";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
@@ -22,7 +21,6 @@ export default [
   pluginReact.configs.flat["jsx-runtime"],
   eslintConfigPrettier,
   eslintPluginPrettierRecommended,
-  ...storybook.configs["flat/recommended"],
   // 2024/10/23 这里的rules不能写在上面，否则会被覆盖
   {
     rules: {
--- packages/api/src/apis/dialog.ts
+++
@@ -1,5 +0,0 @@
-import { api } from "..";
-
-export const Dialog = {
-  show: api("openDialog"),
-};
